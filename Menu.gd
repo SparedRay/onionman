b31252e278412ed18878bf97fede6567947c2a67
Alfredo Cabello <icehealt.alf@gmail.com>
2017-07-19 09:28:40 -0400

    Menu inicial

---
+++ Menu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var scene_moved = false
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	if (Input.is_action_pressed("start_game") and not scene_moved):
+		print("Pulsado")
+		scene_moved = true
+		get_node("Button/Blink").play("Pressed")
+		get_node("Fade").play("Fade")
+
+func _move_scene():
+	get_tree().change_scene("res://Game.tscn")
